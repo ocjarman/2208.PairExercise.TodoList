1c45f32bae1907ff8ef78698eee8eb33db8b2727
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-10-27 17:28:36 -0500

    Quick fix

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5444/todos',
+  process.env.DATABASE_URL || 'postgres://localhost:5432/todos',
   {
     logging: false
   }
